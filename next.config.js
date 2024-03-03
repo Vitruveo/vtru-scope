31ecd3de9c2626d02e23202b9552f8a2ab9590e6
Nik Kalyani <nik@kalyani.com>
2024-03-03 11:08:04 -0800

    Fix deployment issue

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
     "@mui/icons-material": {
       transform: "@mui/icons-material/{{member}}",
     },
-    // TODO: Consider enabling modularizeImports for material when https://github.com/mui/material-ui/issues/36218 is resolved
-    // '@mui/material': {
-    //   transform: '@mui/material/{{member}}',
-    // },
+    webpack: (config) => {
+      config.externals.push("pino-pretty", "lokijs", "encoding");
+      return config;
+    },
   },
   
 };
