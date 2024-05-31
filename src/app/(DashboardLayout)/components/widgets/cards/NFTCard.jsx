fdb2c39898a99e8a9eed59c181ea44badd1fb1c4
Nik Kalyani <nik@kalyani.com>
2024-05-31 08:41:52 -0700

    video placeholder

--- src/app/(DashboardLayout)/components/widgets/cards/NFTCard.jsx
+++ src/app/(DashboardLayout)/components/widgets/cards/NFTCard.jsx
@@ -12,7 +12,7 @@ const NFTCard = ({ nft }) => {
         md={4}
         sm={6} 
         display="flex" key={nft[0]}>
-              <a href={`${nft.storeUrl}`} target="_new"><img src={`${nft.previewUrl}`} alt={`${nft.name}`} style={{width: '100%' }} id={`img-${nft.key}`} /></a>
+              <a href={`${nft.storeUrl}`} target="_new"><img src={`${nft.previewUrl.indexOf('.mp4') > -1 ? 'images/blank.png' : nft.previewUrl }`} alt={`${nft.name}`} style={{width: '100%' }} id={`img-${nft.key}`} /></a>
         </Grid>
       )
   }
