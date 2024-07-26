2b62c5758d998ad6dfb84449328b2e19eb706bfb
Nik Kalyani <nik@kalyani.com>
2024-07-26 06:13:32 -0700

    Smaller art images

--- src/app/(pages)/components/widgets/cards/VtruNFTCard.jsx
+++ src/app/(pages)/components/widgets/cards/VtruNFTCard.jsx
@@ -9,9 +9,9 @@ const VtruNFTCard = ({ nft }) => {
   return (
         <Grid item   
         xs={12}
-        lg={4}
-        md={4}
-        sm={6} 
+        lg={3}
+        md={3}
+        sm={4} 
         display="flex" key={nft[0]}>
               <a href={`${nft.storeUrl}`} target="_new"><img src={`${nft.previewUrl.indexOf('.mp4') > -1 ? '/images/blank.png' : nft.previewUrl }`} alt={`${nft.name}`} style={{width: '100%' }} id={`img-${nft.key}`} /></a>
         </Grid>
