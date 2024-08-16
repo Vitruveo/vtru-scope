76910879274cc0a72114d63f29af2988c7d192fa
Matheus Paice <matheus.paice@jbtec.com.br>
2024-08-16 12:15:45 -0300

    feat: add pagination nft

--- src/app/(pages)/components/widgets/cards/VtruNFTCard.jsx
+++ src/app/(pages)/components/widgets/cards/VtruNFTCard.jsx
@@ -1,21 +1,43 @@
-import {
-  Grid,
-} from '@mui/material';
-
+import { Grid } from "@mui/material";
 
 const VtruNFTCard = ({ nft }) => {
+  if (!Object.hasOwn(nft, "previewUrl")) return <></>;
 
-  if (!Object.hasOwn(nft, 'previewUrl')) return <></>;
-  return (
-        <Grid item   
-        xs={12}
-        lg={3}
-        md={3}
-        sm={4} 
-        display="flex" key={nft[0]}>
-              <a href={`${nft.storeUrl}`} target="_new"><img src={`${nft.previewUrl.indexOf('.mp4') > -1 ? '/images/blank.png' : nft.previewUrl }`} alt={`${nft.name}`} style={{width: '100%' }} id={`img-${nft.key}`} /></a>
-        </Grid>
-      )
+  if (nft.previewUrl.indexOf(".mp4") > -1) {
+    return (
+      <Grid item xs={12} lg={3} md={3} sm={4} display="flex" key={nft[0]}>
+        <a href={`${nft.storeUrl}`} target="_new">
+          <video
+            src={`${nft.previewUrl}`}
+            alt={`${nft.name}`}
+            style={{ width: "100%", height: "100%" }}
+            id={`img-${nft.key}`}
+            controls={false}
+            autoPlay
+            loop
+            muted
+          />
+        </a>
+      </Grid>
+    );
   }
+
+  return (
+    <Grid item xs={12} lg={3} md={3} sm={4} display="flex" key={nft[0]}>
+      <a href={`${nft.storeUrl}`} target="_new">
+        <img
+          src={`${
+            nft.previewUrl.indexOf(".mp4") > -1
+              ? "/images/blank.png"
+              : nft.previewUrl
+          }`}
+          alt={`${nft.name}`}
+          style={{ width: "100%" }}
+          id={`img-${nft.key}`}
+        />
+      </a>
+    </Grid>
+  );
+};
 
 export default VtruNFTCard;
