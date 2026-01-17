845a3f8f2a94969529bee9f2cc682ca29dc368d3
Nik Kalyani <nik@kalyani.com>
2026-01-16 23:15:57 -0800

    New home

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -26,16 +26,23 @@ export default function Dashboard() {
       <h2 style={{width: '100%', marginTop: '30px', lineHeight: '32px'}}>
         The Vitruveo protocol has been upgraded to the Shanghai build. All features are live and all prior wallets and contracts are on-chain. This upgrade features:
         <ul style={{listStyleType: "none"}}>
-          <li>✨ 12 unique and innovative built-in protocol contracts for AI and Game developers</li>
-          <li>✨ New tokenomics to address circulating supply</li>
-          <li>✨ AI-first agentic workflows driven by smart contracts</li>
-          <li>✨ Gemini AI chat access to all chain data via custom MCP server</li>
-          <li>✨ EVM-first smart contract triggers</li>
-          <li>✨ New monetization model for Validators</li>
-          <li>✨ NEW: Custom Vitruveo Multisig Wallet</li>
+          <li>✅ 12 unique and innovative built-in protocol contracts for AI and Game developers</li>
+          <li>✅ New tokenomics to address circulating supply</li>
+          <li>✅ AI-first agentic workflows driven by smart contracts</li>
+          <li>✅ Gemini AI chat access to all chain data via custom MCP server</li>
+          <li>✅ EVM-first smart contract triggers</li>
+          <li>✅ Monetization model for Validators</li>
         </ul>
       </h2>
-
+       <h2 style={{width: '100%', marginTop: '30px', lineHeight: '32px'}}>
+          What's New
+          <ul style={{listStyleType: "none"}}>
+            <li>✨ <a href="/assets/multisig" style={{textDecoration: "underline", color: "white"}}>Custom Vitruveo Multisig Wallet</a></li>
+            <li>✨ <a href="https://www.verticalfnd.com" target="_new" style={{textDecoration: "underline", color: "white"}}>Vertical Foundation landing page</a></li>
+            <li>✨ <a href="https://kalyani.com" target="_new" style={{textDecoration: "underline", color: "white"}}>HOST (HTTP Outbound Service Trigger) Whitepaper</a></li>
+            <li>✨ <a href="https://kalyani.com/trend.html" target="_new" style={{textDecoration: "underline", color: "white"}}>Trend Protocol Yellowpaper</a></li>
+          </ul>
+       </h2>
     </PageContainer>
   );
 }
